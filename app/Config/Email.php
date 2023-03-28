cc27f937da7473c95a543bb9720a61f49adcbe3f
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2023-03-28 09:40:05 +0700

    Update Email.php

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -48,7 +48,7 @@ class Email extends BaseConfig
     /**
      * SMTP Timeout (in seconds)
      */
-    public int $SMTPTimeout = 5;
+    public int $SMTPTimeout = 60;
 
     /**
      * Enable persistent SMTP connections
